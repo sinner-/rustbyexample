2529995ced6106882bf4fd7ada5b98d56d91de9b
Sina Sadeghi <sina.sa@gmail.com>
2018-05-21 21:47:52 +1000

    continue 08

---
+++ 08/foranditerators.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    let names2 = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names2.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    let mut names3 = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names3.iter_mut() {
+        match name {
+            &mut "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+}
